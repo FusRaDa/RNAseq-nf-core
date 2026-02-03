9c149ea4b09bd7dd993ad91362770a8409b7b0b9
Matthew Rada <mrada@FusRaDa.mynetworksettings.com>
2026-02-03 14:52:39 -0500

    updated r script

--- view_summerized_experiment.R
+++ view_summerized_experiment.R
@@ -1,5 +1,6 @@
 library(SummarizedExperiment)
 library(ggplot2)
+library(ggpubr)
 
 # The objective of this script is to generate gene expression of all six samples
 # and produce 6 tables ranking expresion levels of these genes
@@ -17,36 +18,79 @@ assayNames(rds_file)
 colData(rds_file)
 
 # save in variable
-tpm = assay(rds_file, "salmon.merged.gene_tpm")
+tpm = assay(rds_file, "salmon.merged.gene_counts")
 
-# Filter sum of TPM per row to greater than 0 and turn to a dataframe
+# Filter sum of counts per row that is greater than 1000
 row_sums <- rowSums(tpm)
-tpm_filtered <- tpm[row_sums != 0, ]
+tpm_filtered <- tpm[row_sums > 1000, ]
 tpm_df <- data.frame(tpm_filtered)
 
-# Create dataframes for all 6 samples
-sample_1 <- head(tpm_df[order(tpm_df$ENCSR000COQ1, decreasing = TRUE), ][1])
-sample_2 <- head(tpm_df[order(tpm_df$ENCSR000COQ2, decreasing = TRUE), ][2])
-sample_3 <- head(tpm_df[order(tpm_df$ENCSR000COR1, decreasing = TRUE), ][3])
-sample_4 <- head(tpm_df[order(tpm_df$ENCSR000COR2, decreasing = TRUE), ][4])
-sample_5 <- head(tpm_df[order(tpm_df$ENCSR000CPO1, decreasing = TRUE), ][5])
-sample_6 <- head(tpm_df[order(tpm_df$ENCSR000CPO2, decreasing = TRUE), ][6])
-
-
 # Create a vertical bar plot first
-ggplot(sample_1, aes(x = reorder(row.names(sample_1), ENCSR000COQ1), y = ENCSR000COQ1)) +
+b1 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000COQ1), y = ENCSR000COQ1, fill = row.names(tpm_df))) +
   geom_bar(stat = "identity") +
-  
-  # Flip the coordinates to make it horizontal
+  labs(
+    title = "ENCSR000COQ1",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
   coord_flip()
 
-
-# Create a vertical bar plot first
-ggplot(sample_2, aes(x = reorder(row.names(sample_2), ENCSR000COQ2), y = ENCSR000COQ2)) +
+b2 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000COQ2), y = ENCSR000COQ2, fill = row.names(tpm_df))) +
   geom_bar(stat = "identity") +
-  
-  # Flip the coordinates to make it horizontal
+  labs(
+    title = "ENCSR000COQ2",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
   coord_flip()
 
+b3 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000COR1), y = ENCSR000COR1, fill = row.names(tpm_df))) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "ENCSR000COR1",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
+  coord_flip()
 
+b4 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000COR2), y = ENCSR000COR2, fill = row.names(tpm_df))) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "ENCSR000COR2",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
+  coord_flip()
+
+b5 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000CPO1), y = ENCSR000CPO1, fill = row.names(tpm_df))) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "ENCSR000CPO1",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
+  coord_flip()
+
+b6 <- ggplot(tpm_df, aes(x = reorder(row.names(tpm_df), ENCSR000CPO2), y = ENCSR000CPO2, fill = row.names(tpm_df))) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "ENCSR000CPO2",
+    y = NULL,
+    x = NULL
+  ) +
+  theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank()) + 
+  coord_flip()
+
+figure <- ggarrange(b1, b2, b3, b4, b5, b6, ncol = 2, nrow = 3, common.legend = TRUE, legend = "bottom")
+
+annotated_figure <- annotate_figure(
+  figure,
+  top = text_grob("Sample Top Gene Expression (Raw Counts)", color = "black", face = "bold", size = 16)
+)
+annotated_figure
 
